0b4de7825b86adb95723acc4cd00a3bef6a981d9
santhoshagganur <agganursanthosh@gmail.com>
2022-12-31 06:30:58 +0000

    button not worked

---
+++ src/components/ClickCounter/index.js
@@ -0,0 +1,32 @@
+/* eslint-disable react/button-has-type */
+// Write your code here
+import {Component} from 'react'
+import './index.css'
+
+class ClickCounter extends Component {
+  state = {count: 0}
+
+  onIncrement = () => {
+    this.setState(prevState => ({count: prevState.count + 1}))
+  }
+
+  render() {
+    const {count} = this.state
+    return (
+      <div className="bg-container">
+        <h1 className="heading">
+          The Button has been clicked <span className="timer">{count} </span>
+          times
+        </h1>
+        <p className="description"> Click the button to increase the count </p>
+        <div>
+          <button type="button" className="button" onClick={this.onIncrement}>
+            Click Me!
+          </button>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default ClickCounter
